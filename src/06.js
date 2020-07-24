67a5baa794c8cfcdc1d60bdd495cc104beeb5275
Taufik Rahadi <taufikrahadi1@gmail.com>
2020-07-25 02:51:27 +0700

    updated

--- src/06.js
+++ src/06.js
@@ -1,3 +1,3 @@
-const repeatString = (str, number) => str.repeat(number)
+const repeatString = (str, number = 1) => str.repeat(number)
 
 module.exports = repeatString
--- src/07.js
+++ src/07.js
@@ -1,4 +1,4 @@
-const repeatLetter = (text, number) => text.split("").map(str => (str.match(/[a-zA-Z0-9]/) ? str.repeat(number) : str)).join("")
+const repeatLetter = (text, number = 1) => text.split("").map(str => (str.match(/[a-zA-Z0-9]/) ? str.repeat(number) : str)).join("")
 
 module.exports = repeatLetter
 console.log(repeatLetter('Hello World!', 3));
