7867852a7143f195b6982e6d2fa98af30b49378f
Taufik Rahadi <taufikrahadi1@gmail.com>
2020-07-28 22:36:45 +0700

    getUserById without try catch, change keyname users => user

--- 02.js
+++ 02.js
@@ -3,12 +3,8 @@ const axios = require('axios')
 axios.default.defaults.baseURL = 'https://jsonplaceholder.typicode.com'
 
 const getUserById = async id => {
-    try {
-        const request = await axios.get('/users/' + id)
-        return request.data
-    } catch(e) {
-        throw Error(e.statusText)
-    }
+    const request = await axios.get('/users/' + id)
+    return request.data
 }
 
 const getPosts = async () => {
@@ -16,7 +12,7 @@ const getPosts = async () => {
         const request = await axios.get('/posts')
         return Promise.all(request.data
             .map(async post => { 
-                post['users'] = await getUserById(post.userId) 
+                post['user'] = await getUserById(post.userId) 
                 return post 
             }))
                 .then(posts => Promise.resolve(posts))
