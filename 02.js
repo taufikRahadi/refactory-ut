529deeb4e6dd9396464f2e307eb8d813140accbf
Taufik Rahadi <taufikrahadi1@gmail.com>
2020-07-28 23:26:29 +0700

    tambah dokumentasi biar bisa dibaca ulang

--- 02.js
+++ 02.js
@@ -1,21 +1,28 @@
 const axios = require('axios')
-
+// set default baseUrl
 axios.default.defaults.baseURL = 'https://jsonplaceholder.typicode.com'
 
 const getUserById = async id => {
+    // send get request to url
     const request = await axios.get('/users/' + id)
+    // return response data
     return request.data
 }
 
 const getPosts = async () => {
+    // make try catch to handle error
     try {
         const request = await axios.get('/posts')
+        // create promise from fetched posts
         return Promise.all(request.data
+            // inject user key value to post
             .map(async post => { 
                 post['user'] = await getUserById(post.userId) 
                 return post 
             }))
+            // promise resolve send posts
                 .then(posts => Promise.resolve(posts))
+            // promise reject send err
                 .catch(err => Promise.reject(err))
     } catch(e) {
         throw Error(e)
