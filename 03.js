94a192476a66682696b3f1c95ee8a7413922c646
Taufik Rahadi <taufikrahadi1@gmail.com>
2020-07-28 23:29:14 +0700

    tambah dokumentasi juga di nomor 3 biar ngga lupa 3h3h3

--- 03.js
+++ 03.js
@@ -1,8 +1,11 @@
 const print = () => {
+    // buat promise
     return new Promise((resolve,reject) => {
+        // loop
         for (let i = 1; i <= 3; i++) {
             setTimeout(() => {
-                (i <= 3) ? resolve('done') :reject('fail')
+                // if i lower or equal to 3 resolve promise if false reject promise
+                (i <= 3) ? resolve('done') : reject('fail')
                 console.log(i)
             }, 1000);
         }
