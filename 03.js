2a2fd0beed7724374a64f11a5a87f4c24cb76a94
Taufik Rahadi <taufikrahadi1@gmail.com>
2020-07-28 23:22:33 +0700

    nomor 3 bener hehe

--- 03.js
+++ 03.js
@@ -2,17 +2,14 @@ const print = () => {
     return new Promise((resolve,reject) => {
         for (let i = 1; i <= 3; i++) {
             setTimeout(() => {
-                resolve(i)
+                (i <= 3) ? resolve('done') :reject('fail')
+                console.log(i)
             }, 1000);
         }
-    }).then(i => {
-        Promise.resolve(i)
-            .then(print())
-            .then(console.log.bind(console))
     })
 }
 
-(() => {
-    const out = print()
-    console.log(out);    
+(async () => {
+    const out = await print()
+    console.log(out)
 })()
