33c1188c300c4e09bfa0f2c33bbdc5bde31ce2e6
Taufik Rahadi <taufikrahadi1@gmail.com>
2020-07-29 18:19:48 +0700

    tambah seeder

---
+++ seeders/20200729111707-task-filled.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('Tasks', [{
+      text: 'Buat tugas',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      text: 'Masak Nasi',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      text: 'Mandiin Kucing',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
