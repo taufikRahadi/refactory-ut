bbf697da6631eaa637d7e37e3852d08f1181be46
Taufik Rahadi <taufikrahadi1@gmail.com>
2020-07-29 16:10:43 +0700

    update 03

--- 03.js
+++ 03.js
@@ -3,11 +3,10 @@ const print = () => {
     return new Promise((resolve,reject) => {
         // loop
         for (let i = 1; i <= 3; i++) {
+            console.log(i)
             setTimeout(() => {
-                // if i lower or equal to 3 resolve promise if false reject promise
-                (i <= 3) ? resolve('done') : reject('fail')
-                console.log(i)
-            }, 1000);
+                resolve("done")
+            }, 1000)
         }
     })
 }
