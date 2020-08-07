98a08cbd3ce9c5202eb1bb8badcca4eb4451a58a
Taufik Rahadi <taufikrahadi1@gmail.com>
2020-08-08 00:20:07 +0700

    create authorization middleware to validate token

---
+++ src/middleware/authorization.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const jwtSecret = process.env.JWT_SECRET
+const response = require('../utils/response-template')
+
+module.exports = (req, res, next) => {
+    if(req.headers['authorization'])  {
+        const token = req.headers['authorization'].split(' ')[1]
+        if(!token) return res.status(401).json(response('fail', 'unauthenticated'))
+    
+        const verify = jwt.verify(token, jwtSecret)
+        if (verify) {
+            req.token = token
+            return next()
+        } else {
+            return res.status(500).json(response('fail', 'failed to authenticate'))
+        }
+    } else {
+        return res.status(403).json(response('fail', 'this endpoint need a token'))
+    }
+}
