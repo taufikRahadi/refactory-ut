af489d087e216e723049525627452d3ee77b4010
Taufik Rahadi <taufikrahadi1@gmail.com>
2020-08-08 00:19:17 +0700

    auth controller complete

---
+++ src/controllers/auth-controller.js
@@ -0,0 +1,65 @@
+require('dotenv').config()
+const { Author } = require('../models')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const response = require('../utils/response-template')
+const secret = process.env.JWT_SECRET
+
+class AuthController {
+    static async signup (req, res) {
+        const { username, email, password, photo } = req.body
+        try {
+            const hash = await bcrypt.hash(password, 12)
+            const author = await Author.create({
+                username: username,
+                email: email,
+                password: hash,
+                photo: photo
+            })
+            res.status(201).json(response('success', 'signup success', author))
+        } catch (err) {
+            res.status(500).json(response('fail', err))
+        }
+    }
+
+    static async login (req, res) {
+        const { username, password } = req.body
+        try {
+            const user = await Author.findOne({
+                where: {
+                    username: username
+                },
+            })
+
+            if (user) {
+                const compare = await bcrypt.compare(password, user.password)
+                if (compare) {
+                    const token = jwt.sign({ authorId: user.id }, secret, {
+                        expiresIn: '7d'
+                    })
+                    res.status(200).json(response('success', 'login success', {token}))
+                } else res.status(401).json(response('fail', 'wrong password'))
+            } else res.status(404).json(response('fail', 'user not found'))
+        } catch (err) {
+            res.status(500).json(response('fail', err))
+        }
+    }
+
+    static async currentUser (req, res) {
+        const token = req.token
+        const userId = jwt.decode(token, secret).authorId
+        try {
+            const user = await Author.scope('withoutPassword').findOne({
+                where: {
+                    id: userId
+                }
+            })
+            if (user) res.status(200).json(response('success', 'current user', {user, token: token}))
+            else res.status(401).json(response('fail', 'token not valid'))
+        } catch (err) {
+            res.status(500).json(response('fail', err))
+        }
+    }
+}
+
+module.exports = AuthController
