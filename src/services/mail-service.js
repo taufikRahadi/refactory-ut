cbf15bb785af80ff0e40729e1d5026c1bf79b022
Taufik Rahadi <taufikrahadi1@gmail.com>
2020-08-13 01:00:21 +0700

    send mail service

---
+++ src/services/mail-service.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const nodemailer = require('nodemailer')
+const config = require('../../config/nodemailer-config')
+const handlebars = require('handlebars')
+
+const readHTMLFile = require('../helpers/readHTMLFiles')
+
+const templatePath = __dirname + '/../views/email/'
+
+async function sendMailRegister(payload) {  
+    const transporter = await nodemailer.createTransport(config)
+
+    readHTMLFile(templatePath + 'register-template.html', (err, html) => {
+      if(err) throw Error(err.message)
+      const template = handlebars.compile(html)
+      const replacements = {
+        fullname: payload.fullname,
+        address: payload.address,
+        phone: payload.phone
+      }
+
+      const htmlTemplate = template(replacements)
+      const mail = {
+        to: payload.email,
+        from: process.env.AUTH_EMAIL_USER,
+        subject: 'Registration Success',
+        html: htmlTemplate
+      }
+      transporter.sendMail(mail)
+    })
+  }
+
+  module.exports = sendMailRegister
