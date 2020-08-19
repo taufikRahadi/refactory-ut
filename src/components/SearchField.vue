84152827b22ffeae03e0e672ff52e91735d93b86
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-19 15:10:09 +0700

    selese

---
+++ src/components/SearchField.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="mx-20 p-10">
+        <form @submit.prevent="">
+            <input type="text" class="appearence-none rounded-md w-full p-3 bg-white" placeholder="Search Task">
+            <button></button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
