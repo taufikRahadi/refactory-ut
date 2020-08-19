5b2d9520770ebac55c638dbf25deece1a1419058
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-19 21:20:49 +0700

    update component

--- src/components/SearchField.vue
+++ src/components/SearchField.vue
@@ -1,14 +1,24 @@
 <template>
-    <div class="mx-20 p-10">
-        <form @submit.prevent="">
-            <input type="text" class="appearence-none rounded-md w-full p-3 bg-white" placeholder="Search Task">
-            <button></button>
-        </form>
+    <div class="px-20">
+      <input type="text" v-model="search" class="appearence-none rounded-md w-full p-3 bg-white" placeholder="Search Task">
+      <h3 class="text-gray-500 text-lg ml-3 transition-all duration-500 mt-3">{{ message }}</h3>
     </div>
 </template>
 
 <script>
 export default {
-    
+    // props: ['searchTask'],
+    // props: ['search', 'message'],
+    data: () => ({
+        search: '',
+        message: '',
+    }),
+
+    watch: {
+        search: function () {
+            this.message = 'Sedang Mengetik...'
+            this.$emit('search-task', this.search)
+        },
+    }
 }
 </script>
---
+++ src/components/TaskBox.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="card">
+            <div class="flex justify-center">
+                <img :src="task.thumbnailUrl" :alt="`${task.title}_image`" />
+                <div class="m-5">
+                    <h3 class="font-semibold text-lg">
+                        {{ task.title }}
+                    </h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['task']
+}
+</script>
