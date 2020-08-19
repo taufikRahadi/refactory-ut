aa02f931dab63b1b85bcba11c448c66e6f37c09f
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-19 22:23:15 +0700

    track useer is typing or not

--- src/components/SearchField.vue
+++ src/components/SearchField.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="px-20 animate__animated animate__fadeIn">
       <input type="text" v-model="search" class="appearence-none rounded-md w-full p-3 bg-white" placeholder="Search Task">
-      <h3 class="text-gray-500 text-lg ml-3 transition-all duration-500 mt-3">{{ message }}</h3>
+      <h3 class="text-gray-500 text-lg ml-3 transition-all duration-500 mt-3" v-show="isTyping">{{ message }}</h3>
     </div>
 </template>
 
@@ -13,6 +13,12 @@ export default {
         search: '',
         message: '',
     }),
+
+    computed: {
+        isTyping() {
+            return this.search.length >= 3
+        }
+    },
 
     watch: {
         search: function () {
