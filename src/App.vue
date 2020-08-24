004798b07695ba8b6fc9cfe9e4309d88aefc2ea8
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-24 13:55:53 +0700

    task 3 selesai

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,19 @@
     <search-field :message="message" @search-task="searchTask"></search-field>
 
     <h2 class="font-bold px-5 md:px-20 text-5xl text-gray-700 animate__animated animate__fadeIn animate__delay-1s">Task List</h2>
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 px-5 md:px-20">
+    <span class="text-lg text-gray-700 px-5 md:px-20" v-show="filtered">Filtered Task</span>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mt-5 mb-5 px-5 md:px-20">
       <task-box v-for="task in data" :key="task.id" :task="task"></task-box>
+    </div>
+    <div v-show="filtered">
+      <span class="text-lg text-gray-700 px-5 md:px-20">Unfiltered Task</span>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mt-5 px-5 md:px-20">
+        <task-box 
+          v-for="task in unfilteredData"
+          :key="task.id"
+          :task="task"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -18,6 +29,8 @@ export default {
   data: () => ({
     data: data,
     message: '',
+    unSelectedData: [],
+    filtered: false
   }),
 
   methods: {
@@ -26,9 +39,12 @@ export default {
         this.message = 'Searching Task. . .'
         setTimeout(() => {
           this.data = data.filter(val => val.title.includes(search))
+          this.unfilteredData = data.filter(val => !val.title.includes(search))
           this.message = ''
+          this.filtered = true
         }, 2000)
       } else {
+        this.filtered = false
         this.data = data
       }
     }
