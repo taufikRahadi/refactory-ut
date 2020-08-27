25ba6db9c3d674f6fb14bf15ac44d815f3fc63d7
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-27 23:44:25 +0700

    refactor pagination link and add capitalize filter to card

--- vue-route/src/components/Pagination.vue
+++ vue-route/src/components/Pagination.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="flex justify-end border-b-2">
-        <span class="rounded-full bg-white border-2 shadow px-3 py-1" v-for="index in totalPages" :key="index">
-            <router-link :to="{ name: $route.name, params: { page: index } }">{{ index }}</router-link>
-        </span>
+        <router-link :to="{ name: $route.name, params: { page: index } }" v-for="index in totalPages" :key="index">
+            <span class="rounded-full bg-white border-2 shadow px-3 py-1">
+                {{ index }}
+            </span>
+        </router-link>
     </div>
 </template>
 
