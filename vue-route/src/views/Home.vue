9778b4aa0eb0bd16279e883edd91e0bcfba4f131
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-27 23:23:02 +0700

    home views

--- vue-route/src/views/Home.vue
+++ vue-route/src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="home">
-    <h1 class="text-6xl font-black text-gray-700">Ini Home Page</h1>
-  </div>
+	<div class="home">
+		<div class="grid grid-cols-3 gap-4">
+			<div class="col-span-2 h-screen">
+				<h1 class="px-20 my-10 font-light text-6xl">Ini Home Page</h1>
+			</div>
+			<div class="col-span-1">
+				<side-bar />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
