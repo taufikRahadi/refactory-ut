4aeeaea63ee8e98b5bf7c4b4ad975c37f3172869
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-01 19:36:22 +0700

    simplified main.js

--- simple-wms/src/main.js
+++ simple-wms/src/main.js
@@ -6,28 +6,7 @@ import store from './store'
 import './assets/style.css'
 import './plugins/filters'
 import './plugins/globalComponent'
-import VueProgressBar from 'vue-progressbar'
-import VueTailwindPicker from 'vue-tailwind-picker'
-import VueSweetalert2 from 'vue-sweetalert2';
-
-const options = {
-  color: '#38b2ac',
-  failedColor: '#874b4b',
-  thickness: '5px',
-  transition: {
-    speed: '0.2s',
-    opacity: '0.6s',
-    termination: 300
-  },
-  autoRevert: true,
-  location: 'top',
-  inverse: false
-}
-
-Vue.use(VueProgressBar, options)
-Vue.use(VueTailwindPicker);
-Vue.use(VueSweetalert2)
-
+import './plugins/plugins'
 
 
 Vue.config.productionTip = false
