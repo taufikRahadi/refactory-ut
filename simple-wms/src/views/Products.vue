159bcdd37c7cbdd975b207d1dd0470c3bf93f299
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-01 19:35:27 +0700

    create custom submit action inside products views

--- simple-wms/src/views/Products.vue
+++ simple-wms/src/views/Products.vue
@@ -7,6 +7,8 @@
             :isLoading="isLoading"
             modulename="product"
             @reset-data="resetData"
+            :customSubmitAction="submitAction"
+
         >
             <template v-slot:table-row>
                 <tr :class="index % 2 == 0 ? 'bg-gray-200' : ''" v-for="(product, index) in $store.state.product.products.data" :key="product.id">
@@ -40,7 +42,6 @@
 </template>
 
 <script>
-import { Form } from 'vform'
 export default {
     data: () => ({
         isLoading: true,
@@ -58,8 +59,58 @@ export default {
     }),
 
     methods: {
+        async submitAction () {
+            this.$Progress.start()
+            const formData = new FormData()
+            if (this.$store.state.isEditing) {
+                formData.append('id', this.form.id)
+                formData.append('name', this.form.name)
+                formData.append('stock', this.form.stock)
+                formData.append('price', this.form.price)
+                formData.append('photo', this.form.photo)
+                try {
+                    await this.$store.dispatch('product/updateData', { id: this.form.id, data: formData })
+                    this.$Progress.finish()
+                    this.$swal.fire(
+                        'Success',
+                        'Data Updated',
+                        'success'
+                    )
+                    this.$store.commit('setShowModal', false)
+                } catch (err) {
+                    this.$Progress.fail()
+                    this.$swal.fire(
+                        'Failed',
+                        'Fail Updating Data',
+                        'error'
+                    )
+                }
+            } else {
+                formData.append('name', this.form.name)
+                formData.append('stock', this.form.stock)
+                formData.append('price', this.form.price)
+                formData.append('photo', this.form.photo)
+                try {
+                    await this.$store.dispatch('product/createData', formData)
+                    this.$Progress.finish()
+                    this.$swal.fire(
+                        'Success',
+                        'Data Created',
+                        'success'
+                    )
+                    this.$store.commit('setShowModal', false)
+                } catch (err) {
+                    this.$Progress.fail()
+                    this.$swal.fire(
+                        'Failed',
+                        'Fail Creating Data',
+                        'error'
+                    )
+                }
+            }
+        },
         fillData (data) {
-            this.form = { ...data }
+            this.form = { ...data, photo: data.photo_url }
         },
         resetData () {
             this.form = {
@@ -71,8 +122,5 @@ export default {
         }
     },
 
-    async mounted () {
-        
-    }
 }
 </script>
