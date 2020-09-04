837c72fb959080834b8f3b009ebe95f29c8756f8
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-04 17:25:49 +0700

    nomor 2 selese

---
+++ 02.Convert.py
@@ -0,0 +1,28 @@
+def Convert(bil):
+    angka=["","Satu","Dua","Tiga","Empat","Lima",
+    "Enam","Tujuh","Delapan",
+    "Sembilan","Sepuluh","Sebelas"]
+    n = int(bil)
+    hasil =" "
+    if n >= 0 and n <= 11:
+        hasil = hasil + angka[n]
+    elif n < 20:
+        hasil = Convert(n % 10) + " Belas"
+    elif n < 100:
+        hasil = Convert(n / 10) + " Puluh" + Convert(n % 10)
+    elif n < 200:
+        hasil = " Seratus" + Convert(n - 100)
+    elif n < 1000:
+        hasil = Convert(n / 100) + " Ratus" + Convert(n % 100)
+    elif n == 1000 :
+        hasil = " Seribu"
+    else:
+        hasil="Angka hanya sampai seribu"
+    return hasil
+
+satu = Convert(1)
+belasan = Convert(12)
+puluhan = Convert(30)
+print(satu)
+print(belasan)
+print(puluhan)
