0c2476384389f83d57ddf023c9d624abca1c6294
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-05 07:16:38 +0700

    nomor 6

--- 06.WebScraper.py
+++ 06.WebScraper.py
@@ -1,10 +1,13 @@
 import requests
-from pprint import pprint
 from bs4 import BeautifulSoup
 
 url = 'https://indeks.kompas.com/headline'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-result = soup.find_all('div')
-open('scrape.txt', 'w')
-print(result)
+result = soup.find_all(class_="latest--indeks")
+for result in result:
+    elemt = result.find_all('a', class_="article__link", href=True)
+    title = [t.text for t in elemt]
+    url = [u['href'] for u in elemt]
+    for i in range(len(title)):
+        print({'URL': url[i], 'Title': title[i]})
