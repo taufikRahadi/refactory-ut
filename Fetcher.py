21f10061a932ad5470b31b003a4f586e1e5707ba
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-09 23:07:41 +0700

    nomor 4

--- Fetcher.py
+++ Fetcher.py
@@ -9,3 +9,24 @@ class Fetcher:
             async with session.get(url) as res:
                 data = json.loads(await res.text())
                 return data
+    
+    @staticmethod
+    async def post(url, payload):
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, data=payload) as res:
+                data = json.loads(await res.text())
+                return data
+
+    @staticmethod
+    async def delete(url):
+        async with aiohttp.ClientSession() as session:
+            async with session.delete(url) as res:
+                data = json.loads(await res.text())
+                return data
+    
+    @staticmethod
+    async def update(url, payload):
+        async with aiohttp.ClientSession() as session:
+            async with session.patch(url, data=payload) as res:
+                data = json.loads(await res.text())
+                return data
