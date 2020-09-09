6b7aec9e3e2074b570399a58c39bfc22c13cc738
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-09 23:03:39 +0700

    nomor 5 yg bener kata mba harlita

--- combine-user-post.py
+++ combine-user-post.py
@@ -2,26 +2,29 @@ import asyncio
 import json
 import time
 from Fetcher import Fetcher as fetcher
-
-async def fetch_user(id):
+async def fetch_user(id, post):
     user = await fetcher.get(f'https://jsonplaceholder.typicode.com/users/{id}')
-    return user
+    post["user"] = user
+    return post
 
 async def fetch_post():
     posts = await fetcher.get('https://jsonplaceholder.typicode.com/posts')
     return posts
 
-async def combine():
+async def fetch_request():
+    start = time.time()
+    print(f"started at {time.strftime('%X')}")
+    # sinkronus
+    posts = await fetch_post()
     list_task = []
-    
-    for post in await fetch_post():
-        user = asyncio.create_task(fetch_user(post['userId']))
-        list_task.append(user)
-        for task in list_task:
-            post['user'] = await task
-        print(json.dumps(post, indent=4))
-    
+    for post in posts:
+        # asink
+        list_task.append(asyncio.create_task(fetch_user(post["userId"], post)))
+    print(json.dumps([await task for task in list_task], indent=2))
+    end = time.time()
+    print(f"finished at {time.strftime('%X')}")
+    print(f"total time execution : {round(int(end - start))}")
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(combine())
+    loop.run_until_complete(fetch_request())
